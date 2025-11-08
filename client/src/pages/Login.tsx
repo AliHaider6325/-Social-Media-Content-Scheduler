6b6091596e45dc9b7edbd73d848d55ae26634c7c
AliHaider6325 <meharbanali6325@gmail.com>
2025-11-08 13:01:04 +0500

    .i

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -31,13 +31,16 @@ export const Login = () => {
     setError("");
 
     try {
-      const res = await fetch(`${API_URL}/api/auth/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+      const res = await fetch(
+        `https://social-media-content-scheduler.vercel.app/api/auth/login`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
       if (!res.ok) {
         const err = await res.json().catch(() => ({}));
